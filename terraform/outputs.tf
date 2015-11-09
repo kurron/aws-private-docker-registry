2b38c6d1e133c4bb4b6e92e03eb61e01e2d2d4e7
Ron Kurr <kurr@kurron.org>
2015-11-09 19:56:06 +0000

    Everything seems to be working again

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,18 +1,18 @@
-#output "user" {
-#    value = "${aws_iam_access_key.registry_user.user}"
-#}
+output "user" {
+    value = "${aws_iam_access_key.registry_user.user}"
+}
 
-#output "key" {
-#    value = "${aws_iam_access_key.registry_user.id}"
-#}
+output "key" {
+    value = "${aws_iam_access_key.registry_user.id}"
+}
 
-#output "secret-key" {
-#    value = "${aws_iam_access_key.registry_user.secret}"
-#}
+output "secret-key" {
+    value = "${aws_iam_access_key.registry_user.secret}"
+}
 
-#output "key-status" {
-#    value = "${aws_iam_access_key.registry_user.status}"
-#}
+output "key-status" {
+    value = "${aws_iam_access_key.registry_user.status}"
+}
 
 output "elb-dns" {
     value = "${aws_elb.load-balancer.dns_name}"
